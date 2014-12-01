5f1aacac8e34a910352ce0068e08650da696a494
Musa Musa <musakunte@gmail.com>
2014-12-01 03:14:01 +0100

    added Remove folder

--- make-updates.bat
+++ make-updates.bat
@@ -5,5 +5,7 @@ set source=%APPDATA%\viLogged\viLogged-Crone\updates
 set destination=%APPDATA%\viLogged\viLogged-Crone
 xcopy %source% %destination% /y
 
+RD /S /Q %source%
+
 NET START "viLogged Core"
 NET START "viLogged API"
