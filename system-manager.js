7dbe4ce1fa2d26da08b74f26951d1ef1e55af929
Musa Musa <musakunte@gmail.com>
2014-12-06 23:10:49 +0100

    fixed file url

--- system-manager.js
+++ system-manager.js
@@ -1,11 +1,11 @@
 'use strict';
 
 var utility = require('./utility');
-var SETTINGS_FILE = 'system.json';
+var SETTINGS_FILE = utility.DOC_DIR+'/system.json';
 function getSetting() {
   var settings = {};
   if (utility.fileExists(SETTINGS_FILE)) {
-    settings = require('./'+SETTINGS_FILE);
+    settings = JSON.parse(utility.loadFile(SETTINGS_FILE));
   }
   return settings;
 }
