84ee8e6d425793f1f2724d807c01f4ea2eaff3f2
Musa Musa <musakunte@gmail.com>
2014-12-08 02:43:34 +0100

    Update make-updates.bat

--- make-updates.bat
+++ make-updates.bat
@@ -1,11 +1,5 @@
-NET STOP "viLogged Core"
-NET STOP "viLogged API"
 
-set source=%APPDATA%\viLogged\viLogged-Cron\updates\
-set destination=%APPDATA%\viLogged\viLogged-Cron\
+set source="%~dp0"updates\
+set destination="%~dp0"
 ROBOCOPY  %source% %destination% *.* /E
-
-NET START "viLogged Core"
-NET START "viLogged API"
-
 RD /S /Q %source%
