06ab0a713af339e5515d57c6a2797c633eb36da0
Samson Quaye <snaquaye@gmail.com>
2015-04-14 23:50:27 +0100

    added resources for restricted items

---
+++ api/v1/restricted-items/model.js
@@ -0,0 +1,44 @@
+var Sequelize = require('sequelize');
+var connection = require('../../../db');
+
+var Entrance = connection.define('Entrance', {
+  uuid: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    notEmpty: {
+      msg: 'This field is required'
+    }
+  },
+  entrance_name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      is: {
+        args: /^[a-zA-Z0-9_]{3,20}$/,
+        msg: 'Entrance name must contain only alphabetical and numeric characters'
+      },
+      notEmpty: {
+        msg: 'This field is required'
+      }
+    }
+  },
+  modified_by_id: {
+    allowNull: false,
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  },
+  created_by_id: {
+    allowNull: false,
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: true
+    }
+  }
+}, {
+  updatedAt: 'modified',
+  createdAt: 'created'
+});
+
+module.exports = Entrance;
