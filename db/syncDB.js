b25f12c123958383d2d5f90b4fa308cfff386f3d
Musa Musa <musakunte@gmail.com>
2015-04-17 14:24:08 +0100

    alter default user profile

--- db/syncDB.js
+++ db/syncDB.js
@@ -1,6 +1,7 @@
 'use strict';
 var connection = require('./index.js');
 var Users = require('../api/v1/users/model');
+var UserProfile = require('../api/v1/user-profile/model');
 var Visitors = require('../api/v1/visitors/model');
 require('../api/v1/appointments/model');
 require('../api/v1/departments/model');
@@ -10,7 +11,7 @@ var utility = require('../utility');
 
 
 connection
-  .sync()
+  .sync({force: true})
   .complete(function(err) {
     if (!!err) {
       console.log('An error occurred while creating the table:', err)
@@ -18,20 +19,27 @@ connection
       Users.create({
         "username": "admin",
         "password": utility.generatePassword('12345'),
-        "email": "admin@gmail.com",
-        "first_name": "System",
-        "last_name": "Admin",
+        "email": 'admin@gmail.com',
+        "first_name": 'System',
+        "last_name": 'Admin',
         "is_superuser": true,
         "is_staff": true,
-        "is_active": true,
-        "phone": "0809998888",
-        "home_phone": null,
-        "work_phone": null
+        "is_active": true
       }).complete(function(err, user) {
         if (err) {
           console.log(err);
         } else {
+          UserProfile.create({
+            "user_id": user.id,
+            "phone": '08067886565',
+            "home_phone": ''
+          }).complete(function(err, profile) {
+            if (err) {
+              console.log(err);
+            } else {
 
+            }
+          });
         }
       });
     }
