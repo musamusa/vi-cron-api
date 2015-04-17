6d42541fc6109c14ce7312820ef6eb1f586d4515
Musa Musa <musakunte@gmail.com>
2015-04-17 14:20:24 +0100

    added user profile module

---
+++ api/v1/user-profile/index.js
@@ -0,0 +1,16 @@
+var router = require('express').Router();
+var controller = require('./controller');
+
+router.get('/', controller.get);
+
+router.get('/search', controller.search);
+
+router.post('/', controller.create);
+
+router.get('/all', controller.all);
+
+router.get('/:id', controller.getUserProfile);
+
+router.put('/:id', controller.update);
+
+module.exports = router;
