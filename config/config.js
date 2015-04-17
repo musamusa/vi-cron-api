ba0898bee94728507a7b027e632f5da82f058095
Musa Musa <musakunte@gmail.com>
2015-04-17 14:28:30 +0100

    added secrete

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  SALT: '$2a$05$bvIG6Nmid91Mu9RcmmWZfO5HJIMCT8riNW0hEp8f6/FuA2/mHZFpe '
+  SALT: '$2a$05$bvIG6Nmid91Mu9RcmmWZfO5HJIMCT8riNW0hEp8f6/FuA2/mHZFpe ',
+  SECRET: 'my secret for iphase'
 };
